fd04fa6d25c5e8750e709831b1faa15dc8f2d8d3
Quyen Nguyen <ma65p2004@gmail.com>
2018-10-02 13:36:15 -0500

    Setup is Completed
    This set up the backbone for the container

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
 import * as React from "react";
 import { render } from "react-dom";
-import Hello from "./Hello";
+import data from "data";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const styles = {
   fontFamily: "sans-serif",
   textAlign: "center"
 };
+
 const Container = props => {
   return (
-    <div>
-      <div>Plants</div>
-      Container
+    <div className="container col-8" id="container">
+      <div className="row">
+        <Column col_name="Plants" col_rows={[1, 2, 3, 4]} />
+        <Column col_name="Units" col_rows={[]} />
+      </div>
     </div>
   );
 };
+
+const Column = props => {
+  return (
+    <div className="bg-info col-4">
+      <div>{props.col_name} </div>
+      <div>{props.col_rows.map(row => <Row {...row} />)}</div>
+    </div>
+  );
+};
+
+const Row = props => {
+  return <div>Row</div>;
+};
+
 const App = () => (
   <div style={styles}>
     <Container />
