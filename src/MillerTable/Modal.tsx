796e4fb25ac24000e709b6f1dd7757175ce15149
Quyen Nguyen <qvn@outlook.com>
2018-10-16 14:54:47 -0500

    Implemented Modals for ReactStrap

---
+++ src/MillerTable/Modal.tsx
@@ -0,0 +1,55 @@
+/* eslint react/no-multi-comp: 0, react/prop-types: 0 */
+
+import * as React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+interface Props {
+    modalTitle: string;
+    children: React.ReactNode;
+    buttonLabel: string;
+    buttonColor: string;
+    buttonSize: string;
+    keyboard?: boolean;
+    className?: string;
+}
+
+interface States {
+    modal: boolean;
+}
+
+class ModalExample extends React.Component<Props, States> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      modal: false
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState({
+      modal: !this.state.modal
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button size={this.props.buttonSize} color={this.props.buttonColor} onClick={this.toggle}>{this.props.buttonLabel}</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+          <ModalHeader toggle={this.toggle}>{this.props.modalTitle}</ModalHeader>
+          <ModalBody>
+              {this.props.children}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
+}
+
+export default ModalExample;
